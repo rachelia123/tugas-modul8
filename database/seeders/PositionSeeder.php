8a5b470562200f17ffa75b66347f052659f37603
adityaeks <mohadityaekosaputra11@gmail.com>
2024-06-26 15:48:46 +0700

    first commit

---
+++ database/seeders/PositionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Position;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PositionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Position::factory()->count(5)->create();
+    }
+}
